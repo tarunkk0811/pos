3f64501e9ab43d6063284b83c03225b31ddb9be2
tarunkk0811 <tarunkk0811>
2021-04-06 17:17:00 +0530

    Companies scene

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,20 +1,24 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Sample.fxml"));
+			Parent root = FXMLLoader.load(getClass().getResource("Sample.fxml"));
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
+			primaryStage.setMaximized(true);
+			
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
--- src/application/SampleController.java
+++ src/application/SampleController.java
@@ -1,5 +1,16 @@
 package application;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
 public class SampleController {
+	 @FXML
+	 private Button sel;
+	
+	@FXML
+	void print(ActionEvent event) {
+		System.out.print("Selected company");
+	}
 	
 }
